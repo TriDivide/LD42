097ecaac3d31b57828582f68f9dc987c7a66a6d4
TriDivide <liamsmitha@gmail.com>
2018-08-12 11:20:27 +0100

    - got rotation and movement speed working

---
+++ LD42/Assets/2D puzzle platformer/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static float MovementSpeed = 0f;
+
+    public GameObject RotateButton;
+    public GameObject PowerButton;
+    public Text PowerIndicator;
+
+    public static bool IsRotating = true;
+    private bool IncrementPower = false;
+
+    public static bool StartMoving = false;
+
+    public static float power = 0f;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(IncreasePower());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    IEnumerator IncreasePower() {
+        while (true) {
+            if (IncrementPower) {
+
+                MovementSpeed += 1;
+                Debug.Log(MovementSpeed);
+                PowerIndicator.text = MovementSpeed.ToString();
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+
+
+    public void StopRotating() {
+        IsRotating = false;
+        RotateButton.SetActive(false);
+        IncrementPower = true;
+    }
+
+    public void SetPower() {
+        StartMoving = true;
+        IncrementPower = false;
+        PowerButton.SetActive(false);
+    }
+}
---
+++ LD42/Assets/2D puzzle platformer/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public float RotationSpeed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameManager.IsRotating) {
+            transform.Rotate(Vector3.forward * -RotationSpeed);
+        }
+		if (GameManager.StartMoving) {
+            gameObject.GetComponent<Rigidbody>().velocity = transform.forward * GameManager.MovementSpeed;
+
+        }
+    }
+}
