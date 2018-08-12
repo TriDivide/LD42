6ab0610a446ac0d8ad1d2eb4996bfd0c43679626
TriDivide <liamsmitha@gmail.com>
2018-08-12 16:39:52 +0100

    - got movement working properly

--- LD42/Assets/2D puzzle platformer/Scripts/PlayerMovement.cs
+++ LD42/Assets/2D puzzle platformer/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour {
 
     public float RotationSpeed = 5f;
 
-    public Sprite self;
+
+    public float strength = 400f;
 
 
 	// Use this for initialization
@@ -20,18 +21,38 @@ public class PlayerMovement : MonoBehaviour {
         if (GameManager.IsRotating) {
             transform.Rotate(Vector3.forward * -RotationSpeed);
         }
-		if (GameManager.StartMoving) {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(transform.right * GameManager.MovementSpeed, ForceMode2D.Impulse);
 
+       // }
+
+
+    }
+
+    private void FixedUpdate() {
+        if (GameManager.StartMoving) {
+       //     Quaternion rot = transform.rotation;
+       //
+         //   float z = rot.eulerAngles.z;
+
+           // rot = Quaternion.Euler(0, 0, z);
+            //transform.rotation = rot;
+
+            var finalAngle = transform.localEulerAngles;
+
+            Debug.Log("Angle: " + finalAngle.z);
+
+            // gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.right * GameManager.MovementSpeed, ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.up * 1f, ForceMode2D.Impulse);
         }
-
-
     }
 
     void OnCollisionEnter2D(Collision2D col) {
         Debug.Log("collided");
         if (col.gameObject.tag == "Danger") {
             Destroy(gameObject);
+        }
+        if (col.gameObject.tag == "Finish") {
+            Destroy(gameObject);
+            Debug.Log("You win");
         }
     }
 }
